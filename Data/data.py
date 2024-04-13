ef1dd42e196b8409dd681cbf167de7c03bd7cafd
Divyadinesh12 <149932116+Divyadinesh12@users.noreply.github.com>
2024-04-13 14:14:33 +0530

    Add files via upload

---
+++ Data/data.py
@@ -0,0 +1,16 @@
+
+"""
+This class is used to contain all the data that are required to perform the testing for the orange HRM  page
+"""
+
+
+class WebData:
+    def __init__(self):
+        self.url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+        self.DashBoardUrl="https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+        self.Reset_Password="Admin"
+        self.Reset_Password_url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/sendPasswordReset"
+        self.User_Name="Admin"
+        self.Pass_Word="admin123"
+        self.title="OrangeHRM"
+
